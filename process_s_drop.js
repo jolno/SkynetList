b6c28b3f4b1c6779aceff434192193f607a306c6
jolno <jamesj8sjshu@gmail.com>
2023-01-01 14:20:03 -0800

    Update process_s_drop.js

--- process_s_drop.js
+++ process_s_drop.js
@@ -11,6 +11,6 @@ https.get("https://www.spamhaus.org/drop/drop.txt", response => {
   });
 });
 
-const filtered = file.replaceAll((;.*), "");
+const filtered = file.replaceAll(\x3B.*, "");
 
 await fs.writeFile(sdrop.txt, filtered, "utf8);
