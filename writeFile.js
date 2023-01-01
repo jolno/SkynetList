c17b8c646dfccf831be7d5b301bc0cfb960e16f5
jolno <jamesj8sjshu@gmail.com>
2023-01-01 15:02:36 -0800

    Create writeFile.js

---
+++ writeFile.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const https = require("https");
+
+const file = fs.createWriteStream("data.txt");
+
+https.get("https://www.spamhaus.org/drop/drop.txt", response => {
+  var stream = response.pipe(file);
+
+  stream.on("finish", function() {
+    console.log("done");
+  });
+});
