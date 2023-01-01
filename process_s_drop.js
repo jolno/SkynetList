9ad2d460e524cfe248ede3cebe4e4526aacfde03
jolno <jamesj8sjshu@gmail.com>
2023-01-01 14:58:10 -0800

    Update process_s_drop.js

--- process_s_drop.js
+++ process_s_drop.js
@@ -14,7 +14,9 @@ https.get("https://www.spamhaus.org/drop/drop.txt", response => {
 
 (async () => {
 
-const filtered = await file.replaceAll(";", "");
+const fileContents = await fs.readFile("data.txt","utf8");
+  
+const filtered = await fileContents.replaceAll(";", "");
   
 await fsp.writeFile("sdrop.txt", filtered, "utf8");
   
