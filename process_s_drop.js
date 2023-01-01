1dad62959361c55eed31fcfb8563aa5f96c062ae
jolno <jamesj8sjshu@gmail.com>
2023-01-01 15:09:55 -0800

    Update process_s_drop.js

--- process_s_drop.js
+++ process_s_drop.js
@@ -2,9 +2,11 @@ const fs = require("fs").promises;
 
 (async () => {
 
-const fileContents = await fs.readFile("data.txt","utf8");
-  
-const filtered = fileContents.replaceAll(";.*", "");
+const file = await fs.readFile("data.txt","utf8");
+
+const regex = [;].*;
+
+const filtered = file.replaceAll(regex, "");
   
 await fs.writeFile("sdrop.txt", filtered, "utf8");
   
