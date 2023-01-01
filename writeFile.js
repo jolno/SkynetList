115163090297ab65b2e5ad6e5f70f769c19f0558
jolno <jamesj8sjshu@gmail.com>
2023-01-01 15:52:14 -0800

    Update writeFile.js

--- writeFile.js
+++ writeFile.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const https = require("https");
 
-const file = fs.createWriteStream("data.txt");
+const file = fs.createWriteStream("drop.txt");
 
 https.get("https://www.spamhaus.org/drop/drop.txt", response => {
   var stream = response.pipe(file);
@@ -10,3 +10,14 @@ https.get("https://www.spamhaus.org/drop/drop.txt", response => {
     console.log("done");
   });
 });
+
+const file2 = fs.createWriteStream("dshield.txt");
+
+https.get("https://www.dshield.org/block.txt", response => {
+  var stream = response.pipe(file2);
+
+  stream.on("finish", function() {
+    console.log("done");
+  });
+});
+
